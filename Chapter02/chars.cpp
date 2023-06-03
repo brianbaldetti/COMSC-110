d44c79bcb57640bd9dc55286c38df166c4f3d654
brianbaldetti <135410817+brianbaldetti@users.noreply.github.com>
2023-06-03 11:03:10 -0700

    Chars Program
    Program test using, creating, and outputing chars in C++.

---
+++ Chapter02/chars.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  Chars
+//
+//  Created by brian on 2/9/22.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char newLine, tab, letterH, letterB;
+    
+    newLine = '\n';
+    tab = '\t';
+    letterH = 75;
+    letterB = 'B';
+    
+    cout << letterH << newLine << tab << letterB << '\n';
+    
+    return 0;
+}
