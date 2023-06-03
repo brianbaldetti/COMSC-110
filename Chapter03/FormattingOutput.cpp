a4b0696348461ba80e02599f0a4595a1b6600583
brianbaldetti <135410817+brianbaldetti@users.noreply.github.com>
2023-06-03 11:21:37 -0700

    Formatting Output Program
    Test using the <iomanip> library to do a little more advanced formatting of output.

---
+++ Chapter03/FormattingOutput.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  FormattingOutput
+//
+//  Created by brian on 2/14/22.
+//
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    cout << right;
+    cout << setw(7) << 67;
+    
+    return 0;
+}
