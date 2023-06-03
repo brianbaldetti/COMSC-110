494a447f517834a769779719611e9191688f7fba
brianbaldetti <135410817+brianbaldetti@users.noreply.github.com>
2023-06-03 11:09:59 -0700

    Identifier Test Program
    Program test outputting using the iostream library.

---
+++ Chapter02/IdentifierTest.cpp
@@ -0,0 +1,22 @@
+//
+//  main.cpp
+//  IdentifierTest
+//
+//  Created by brian on 2/8/22.
+//
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    //int 3x;
+    int x3;
+    
+    x = 1;
+    //3x = 2;
+    x3 = 3;
+    
+    cout << "test " << x << endl;
+    return 0;
+}
