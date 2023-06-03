a4d0aa21927b32640fa4df0d938f11db73111d5c
brianbaldetti <135410817+brianbaldetti@users.noreply.github.com>
2023-06-03 11:13:24 -0700

    Strings Program
    Program test creating, using, and outputting strings.

---
+++ Chapter02/Strings.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  Strings
+//
+//  Created by brian on 2/9/22.
+//
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string name, address, phoneNumber;
+    
+    name = "brain";
+    address = "420 st";
+    phoneNumber = "555-555-5555";
+    
+    cout << "name = " << name << endl;
+    cout << "address = " << address << '\n';
+    cout << "phone number = " << phoneNumber << "\n\n";
+    
+    return 0;
+}
