53e3cf0c8664d3f686d4109bcd282c4a1e296937
brianbaldetti <135410817+brianbaldetti@users.noreply.github.com>
2023-06-03 11:05:27 -0700

    Data Types Program
    Program test creating and using different data types in C++.

---
+++ Chapter02/DataTypes.cpp
@@ -0,0 +1,38 @@
+//
+//  main.cpp
+//  DataTypes
+//
+//  Created by brian on 2/8/22.
+//
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    short shortInt;
+    short int sameAsShort;
+    
+    unsigned short unsignedShortInt;
+    unsigned short int sameAsUnsignedShortInt;
+    
+    int longInt;
+    long int sameAsInt;
+    long sameAsLongInt;
+    
+    unsigned unsignedInt;
+    unsigned int sameAsUnsigned;
+    unsigned long sameAsUnsignedInt;
+    unsigned long int sameAsUnsignedLong;
+    
+    long long longestInt;
+    long long int sameAsLongLong;
+    
+    unsigned long long longestUnsignedInt;
+    unsigned long long int sameAsLongestestUnsignedInt;
+    
+    longestInt = 44444444;
+    longestUnsignedInt = -9999;
+    sameAsUnsigned = -20;
+    
+    cout << "\t" << sameAsUnsigned << "\t" << endl;
+}
