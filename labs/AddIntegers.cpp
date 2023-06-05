9f47d4f7b692460db3d66d27280f6b2f99a666ba
Brian <135410817+brianbaldetti@users.noreply.github.com>
2023-06-04 18:22:19 -0700

    Add Integers Program
    Program adds specific integers together.

---
+++ labs/AddIntegers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a , b, c; //Three integers
+    a = 3;
+    b = 4;
+    c = a + b;
+    cout << "The value of c is " << c;
+    
+    return 0;
+}
